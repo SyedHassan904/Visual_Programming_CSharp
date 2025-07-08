2c31c3fa06a2791543341782c221c4d6fc2df5ff
Syed Hassan <syedhassan154@gmail.com>
2025-07-08 18:33:07 +0500

    Initial commit of ButtonChangingPosition

---
+++ ButtonPosChanger/ButtonPosChanger/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ButtonPosChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 128, 0);
+            button1.Font = new Font("Segoe UI", 13F, FontStyle.Bold);
+            button1.Location = new Point(320, 196);
+            button1.Name = "button1";
+            button1.Size = new Size(114, 46);
+            button1.TabIndex = 0;
+            button1.Text = "No";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            button1.MouseEnter += button1_MouseEnter;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            label1.Location = new Point(154, 98);
+            label1.Name = "label1";
+            label1.Size = new Size(309, 48);
+            label1.TabIndex = 1;
+            label1.Text = "Do You Love me?";
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Lime;
+            button2.Font = new Font("Segoe UI", 13F, FontStyle.Bold);
+            button2.Location = new Point(176, 196);
+            button2.Name = "button2";
+            button2.Size = new Size(113, 46);
+            button2.TabIndex = 0;
+            button2.Text = "Yes";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(642, 477);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "LoveMe";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Button button2;
+    }
+}
---
+++ ButtonPosChanger/ButtonPosChanger/Form1.cs
@@ -0,0 +1,36 @@
+﻿namespace ButtonPosChanger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+         
+
+        }
+        Random r = new Random();
+        int w, h, npx, npy;
+
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+
+            w = this.ClientSize.Width;
+            h = this.ClientSize.Height;
+            npx = r.Next(0, w - button1.Width);
+            npy = r.Next(0, h - button1.Height);
+            button1.Location = new Point(npx, npy);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("I Love you too!!!!❤️❤️❤️❤️🥰🥰");
+        }
+    }
+}
